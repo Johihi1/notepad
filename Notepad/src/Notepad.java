b7344be4d0f88bf1027cc3e3c4d9dae5b6aed43a
Johihi1 <54669280+Johihi1@users.noreply.github.com>
2021-02-19 10:15:18 +0100

    la till funktion för saveknappen

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -1,6 +1,22 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeListener;
 
 public class Notepad {
+    private JButton NewFile;
+
+    public Notepad() {
+        NewFile.addPropertyChangeListener(new PropertyChangeListener() {
+        });
+        NewFile.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
+
     public static void main(String[] args) {
         private JTextArea TextArea;
         private JPanel panel1;
