d77e321b7c9a0403288e9fb64ad7a9c9e23072fe
Johihi1 <54669280+Johihi1@users.noreply.github.com>
2021-02-19 10:37:47 +0100

    la till filinläsningsfunktion

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -1,15 +1,46 @@
+/**
+ *Notepad
+ *
+ * Created 2021-02-19
+ * Author Johan Jokijärvi
+ */
+
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.*;
 import java.beans.PropertyChangeListener;
 
 public class Notepad {
     private JButton NewFile;
+    private JButton Save;
+
+    JFileChooser fc = new JFileChooser();
+    int result = fc.showOpenDialog(null);
+    String filename;
+        if (result == JFileChooser.APPROVE_OPTION) {
+        filename = fc.getSelectedFile().getAbsolutePath();
+    } else {
+        filename = "exempel.txt";
+    }
+
+    FileReader fr = null;
+        try {
+        fr = new FileReader(filename);
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+    BufferedReader inFile = new BufferedReader(fr);
+
 
     public Notepad() {
-        NewFile.addPropertyChangeListener(new PropertyChangeListener() {
-        });
         NewFile.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        Save.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
