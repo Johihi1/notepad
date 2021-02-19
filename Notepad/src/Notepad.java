1c41b8de9b692f9320caf2aa4426ff645ef144c6
Johihi1 <54669280+Johihi1@users.noreply.github.com>
2021-02-19 10:04:36 +0100

    initial commit

---
+++ Notepad/src/Notepad.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Notepad {
+    public static void main(String[] args) {
+        private JTextArea TextArea;
+        private JPanel panel1;
+        private JButton NewFile;
+        private JButton Save;
+        private JScrollBar scrollBar1;
+    }
+}
