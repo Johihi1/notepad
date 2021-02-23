e62d1f62fe52b1ed2a6ab3891822227341b99a0c
Johihi1 <54669280+Johihi1@users.noreply.github.com>
2021-02-23 11:13:41 +0100

    slut på lektion

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -10,32 +10,31 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Notepad {
+    public static void main(String[] args) {
     private JButton newfilebutton;
     private JButton savefilebutton;
     private JButton openFileButton;
 
     public Notepad() {
-        newfilebutton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-            }
-        });
-        savefilebutton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-            }
-        });
-        openFileButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-            }
-        });
-    }
-
-    public static void main(String[] args) {
+            newfilebutton.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
+                }
+            });
+            savefilebutton.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
+                }
+            });
+            openFileButton.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
+                }
+            });
+        }
         private JTextArea TextArea;
         private JPanel panel1;
         private JScrollBar scrollBar1;
